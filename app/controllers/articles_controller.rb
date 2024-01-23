aef288c729488457bd3089143b861f32cea7a451
Arnaud Lahaut <arnaudlht@gmail.com>
2024-01-23 16:56:27 +0100

    Last push

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
